93dd4aba049b8141e1dc01f2a692251251eb5164
DclFabienLycan <dcl.fabienb@18pixel.fr>
2018-11-08 18:26:23 +0100

    first php down, just miss a little isset

---
+++ tablemultiplication/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Table de multiplication</title>
+</head>
+
+<body>
+    <header>
+        <h1>Table de multiplication</h1>
+    </header>
+    <div id="container">
+        <form name="table" method="GET" action="#">
+            <select name="listeTable" id="lTable">
+                <option name="listeTable">Choose</option>
+                <option name="listeTable" value="0">Table de multiplication x0</option>
+                <option name="listeTable" value="1">Table de multiplication x1</option>
+                <option name="listeTable" value="2">Table de multiplication x2</option>
+                <option name="listeTable" value="3">Table de multiplication x3</option>
+                <option name="listeTable" value="4">Table de multiplication x4</option>
+                <option name="listeTable" value="5">Table de multiplication x5</option>
+                <option name="listeTable" value="6">Table de multiplication x6</option>
+                <option name="listeTable" value="7">Table de multiplication x7</option>
+                <option name="listeTable" value="8">Table de multiplication x8</option>
+                <option name="listeTable" value="9">Table de multiplication x9</option>
+                <option name="listeTable" value="10">Table de multiplication x10</option>
+            </select>
+            <p>Choisir la table à afficher</p>
+            <div id="bouton">
+                <button type="submit">Voir</button>
+            </div>
+            <div id="tableau">
+                <?php include 'table.php'; ?>
+            </div>
+        </form>
+        </div>   
+        
+
+
+    <script src="script.js"></script>
+</body>
+
+</html>
--- tablemultiplication/table.php
+++ tablemultiplication/table.php
@@ -1,32 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php 
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Table de multiplication</title>
-</head>
+$table = $_GET['listeTable'];
 
-<body>
-    <header>
-        <h1>Table de multiplication</h1>
-    </header>
-    <main>
-        <?php
-        $multi0 = $_POST=['x0'];
-        $multi1 = $_POST=['x1'];
-        $multi2 = $_POST=['x2'];
-        $multi3 = $_POST=['x3'];
-        $multi4 = $_POST=['x4'];
-        $multi5 = $_POST=['x5'];
-        $multi6 = $_POST=['x6'];
-        $multi7 = $_POST=['x7'];
-        $multi8 = $_POST=['x8'];
-        $multi9 = $_POST=['x9'];
-        $multi10 = $_POST=['x10'];
-        
-        
-        ?>
-    </main>
+for ($b=0; $b<=30; $b++) {
+    echo $table. 'x' .$b. '=' .$table * $b.'<br>';
+}
+
+?>
