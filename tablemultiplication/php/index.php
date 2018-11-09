318b468f4b8556dc470dcfba1171784a3ae8dc83
DclFabienLycan <dcl.fabienb@18pixel.fr>
2018-11-09 12:02:56 +0100

    first stape end

--- tablemultiplication/php/index.php
+++ tablemultiplication/php/index.php
@@ -16,26 +16,23 @@
     <div id="container">
         <form name="table" method="GET" action="#">
             <select name="listeTable" id="lTable">
-                <option name="listeTable">Choose</option>
-                <option name="listeTable" value="0">Table de multiplication x0</option>
-                <option name="listeTable" value="1">Table de multiplication x1</option>
-                <option name="listeTable" value="2">Table de multiplication x2</option>
-                <option name="listeTable" value="3">Table de multiplication x3</option>
-                <option name="listeTable" value="4">Table de multiplication x4</option>
-                <option name="listeTable" value="5">Table de multiplication x5</option>
-                <option name="listeTable" value="6">Table de multiplication x6</option>
-                <option name="listeTable" value="7">Table de multiplication x7</option>
-                <option name="listeTable" value="8">Table de multiplication x8</option>
-                <option name="listeTable" value="9">Table de multiplication x9</option>
-                <option name="listeTable" value="10">Table de multiplication x10</option>
+                <option name="listeTable" value="0">Table de multiplication x 0</option>
+                <option name="listeTable" value="1">Table de multiplication x 1</option>
+                <option name="listeTable" value="2">Table de multiplication x 2</option>
+                <option name="listeTable" value="3">Table de multiplication x 3</option>
+                <option name="listeTable" value="4">Table de multiplication x 4</option>
+                <option name="listeTable" value="5">Table de multiplication x 5</option>
+                <option name="listeTable" value="6">Table de multiplication x 6</option>
+                <option name="listeTable" value="7">Table de multiplication x 7</option>
+                <option name="listeTable" value="8">Table de multiplication x 8</option>
+                <option name="listeTable" value="9">Table de multiplication x 9</option>
+                <option name="listeTable" value="10">Table de multiplication x 10</option>
             </select>
             <p>Choisir la table à afficher</p>
             <div id="bouton">
                 <button type="submit">Voir</button>
             </div>
-            <div id="tableau">
                 <?php include 'table.php'; ?>
-            </div>
         </form>
         </div>   
         
--- tablemultiplication/php/table.php
+++ tablemultiplication/php/table.php
@@ -1,9 +1,15 @@
 <?php 
 
+if(isset($_GET['listeTable'])){
+ echo  '<div id="tableau">';
 $table = $_GET['listeTable'];
-
+echo 'Table de'.' '. $table. '<br>'.'<br>';
 for ($b=0; $b<=30; $b++) {
-    echo $table. 'x' .$b. '=' .$table * $b.'<br>';
+    echo $table. ' '. 'x'. ' ' .$b.' '. '='.' ' .$table * $b.'<br>';
 }
+echo '</div>';
+}
+
+
 
 ?>
